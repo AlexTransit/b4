78c55fe813ceaac9c1318e570721106200f6e383
Daniel Lavrushin <lavrushin@gmail.com>
2026-04-29 22:39:13 +0200

    feat: conditionally render DeviceActivity component based on available device domains in DashboardPage

--- src/http/ui/src/components/dashboard/Page.tsx
+++ src/http/ui/src/components/dashboard/Page.tsx
@@ -324,30 +324,40 @@ export function DashboardPage() {
 
       <ActiveSets sets={sets} />
 
-      <Grid container spacing={1.5} sx={{ mb: 1.5 }} alignItems="stretch">
-        <Grid size={{ xs: 12, xl: 6 }} sx={{ display: "flex" }}>
-          <Box sx={{ width: "100%" }}>
-            <DeviceActivity
-              deviceDomains={metrics.device_domains}
-              domainTLS={metrics.domain_tls}
-              sets={sets}
-              targetedDomains={targetedDomains}
-              onRefreshSets={refreshSets}
-            />
-          </Box>
-        </Grid>
-        <Grid size={{ xs: 12, xl: 6 }} sx={{ display: "flex" }}>
-          <Box sx={{ width: "100%" }}>
-            <UnmatchedDomains
-              topDomains={metrics.top_domains}
-              domainTLS={metrics.domain_tls}
-              sets={sets}
-              targetedDomains={targetedDomains}
-              onRefreshSets={refreshSets}
-            />
-          </Box>
-        </Grid>
-      </Grid>
+      {(() => {
+        const hasDevices = Object.keys(metrics.device_domains).length > 0;
+        return (
+          <Grid container spacing={1.5} sx={{ mb: 1.5 }} alignItems="stretch">
+            {hasDevices && (
+              <Grid size={{ xs: 12, xl: 6 }} sx={{ display: "flex" }}>
+                <Box sx={{ width: "100%" }}>
+                  <DeviceActivity
+                    deviceDomains={metrics.device_domains}
+                    domainTLS={metrics.domain_tls}
+                    sets={sets}
+                    targetedDomains={targetedDomains}
+                    onRefreshSets={refreshSets}
+                  />
+                </Box>
+              </Grid>
+            )}
+            <Grid
+              size={{ xs: 12, xl: hasDevices ? 6 : 12 }}
+              sx={{ display: "flex" }}
+            >
+              <Box sx={{ width: "100%" }}>
+                <UnmatchedDomains
+                  topDomains={metrics.top_domains}
+                  domainTLS={metrics.domain_tls}
+                  sets={sets}
+                  targetedDomains={targetedDomains}
+                  onRefreshSets={refreshSets}
+                />
+              </Box>
+            </Grid>
+          </Grid>
+        );
+      })()}
 
       {metrics.connection_rate.length > 0 && (
         <Paper
